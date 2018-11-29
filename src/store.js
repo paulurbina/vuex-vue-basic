b33fdf3dd8c785fb0e3560f62465e579132cb203
tonypera <paulurbian@hotmail.com>
2018-11-28 19:01:22 -0500

    first commit

---
+++ src/store.js
@@ -0,0 +1,44 @@
+import Vue from 'vue'
+import Vuex from 'vuex'
+
+Vue.use(Vuex)
+
+export default new Vuex.Store({
+  state: {
+  	title: 'My custom title',
+  	links: [
+  	  'http://google.com',
+      'http://coursetro.com',
+      'http://youtube.com'
+  	]
+  },
+  mutations: {
+    ADD_LINK:(state, link) => {
+      state.links.push(link)
+    },
+    REMOVE_LINK: (state, link) => {
+      state.links.splice(link,1)
+    },
+    REMOVE_ALL: (state) => {
+      state.links = []
+    }
+  },
+  getters:{
+    countLinks: state => {
+      return state.links.length
+    }
+  },
+  actions: {
+    removeLink: (context, link) => {
+      context.commit("REMOVE_LINK", link)
+    },
+    removeAll ({commit}) {
+      return new Promise((resolve, reject) => {
+        setTimeout(() => {
+          commit('REMOVE_ALL')
+          resolve()
+        }, 1500)
+      })
+    }
+  }
+})
